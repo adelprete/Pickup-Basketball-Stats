e80df6e8ea1988889126141cc21aab96e9cf80bd
Anthony <adelprete87@gmail.com>
2015-07-23 10:58:04 -0400

    added log in link to menu

--- basketball/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url, include
-
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('basketball.views',
-	url(r'^$',		'root',			name='root'),
-}
