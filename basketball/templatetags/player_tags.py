f03c1695463d38e325fdfaff8da5b6b1d41c5899
Anthony <adelprete87@gmail.com>
2015-12-03 09:17:32 -0500

    Adv totals is now a tab on player pages

--- basketball/templatetags/player_tags.py
+++ basketball/templatetags/player_tags.py
@@ -35,6 +35,80 @@ def player_averages(context, player_id):
         return context
 
 
+def calculate_player_overall_dictionaries(context, category, statistics, player_id=None):
+	
+        player = bmodels.Player.objects.get(id=player_id)
+        seasons = bmodels.Season.objects.all()
+
+        tables = OrderedDict()
+        overall_footer = {}
+        # For each game type we create a list of each player's total stats
+        for game_type in bmodels.GAME_TYPES:
+            tables[game_type[1]] = []
+            overall = {}
+            for season in seasons:
+               
+                if player.get_possessions_count(game_type=game_type[0], season_id=season.id) > 0:
+                    season_data = {'title': season.title}
+                    
+                    stats_list = [header['stat'] for header in statistics if header['stat'] != 'gp']
+                    if category == 'totals':
+                        season_data.update(player.get_totals(stats_list, game_type=game_type[0], season=season))
+                    elif category == 'averages':
+                        season_data.update(player.get_averages(stats_list, game_type=game_type[0], season=season))
+                    
+                    # Lastly, count how many games the player played
+                    statlines = player.statline_set.filter(game__game_type=game_type[0], game__date__range=(season.start_date, season.end_date))
+                    season_data['gp'] = statlines.count()
+
+                    tables[game_type[1]].append(season_data)
+
+                    if category == 'totals':
+                        for key, value in season_data.items():
+                            if key is not 'title':
+                                if key in overall:
+                                    overall[key] += value
+                                else:
+                                    overall[key] = value
+                    elif category == 'averages':
+                        overall = player.get_averages(stats_list, game_type=game_type[0])
+                        overall['gp'] = ''
+
+            overall_footer[game_type[1]] = overall
+        return tables, overall_footer
+
+@register.inclusion_tag('players/stats_tab.html', takes_context=True)
+def player_totals(context, player_id):
+        """Returns a dictionary of totals for a player by Season"""
+
+        totals_tables, overall_footer = calculate_player_overall_dictionaries(context, 'totals', headers.totals_statistics[1:],player_id=player_id)
+        
+        context = {
+            'tables': totals_tables,
+            'overall_footer': overall_footer,
+            'category': 'tot',
+            'active_pill': '5on5',
+            'headers': headers.totals_statistics
+            }
+        
+        return context
+
+@register.inclusion_tag('players/stats_tab.html', takes_context=True)
+def player_adv_totals(context, player_id):
+        """Returns a dictionary of totals for a player by Season"""
+
+        totals_tables, overall_footer = calculate_player_overall_dictionaries(context, 'totals', headers.adv_totals_statistics[1:],player_id=player_id)
+        
+        context = {
+            'tables': totals_tables,
+            'overall_footer': overall_footer,
+            'category': 'adv-tot',
+            'active_pill': '5on5',
+            'headers': headers.adv_totals_statistics
+            }
+        
+        return context
+
 def calculate_player_possessions_dictionaries(context, headers, player_id=None, sort_column=""):
     
     seasons = bmodels.Season.objects.all()
@@ -92,64 +166,4 @@ def player_adv_possessions(context, player_id):
         'headers': headers.adv_per_100_statistics,
     }
     return context
-
-def calculate_player_overall_dictionaries(context, category, statistics, player_id=None):
-	
-        player = bmodels.Player.objects.get(id=player_id)
-        seasons = bmodels.Season.objects.all()
-
-        tables = OrderedDict()
-        overall_footer = {}
-        # For each game type we create a list of each player's total stats
-        for game_type in bmodels.GAME_TYPES:
-            tables[game_type[1]] = []
-            overall = {}
-            for season in seasons:
-               
-                if player.get_possessions_count(game_type=game_type[0], season_id=season.id) > 0:
-                    season_data = {'title': season.title}
-                    
-                    stats_list = [header['stat'] for header in statistics if header['stat'] != 'gp']
-                    if category == 'totals':
-                        season_data.update(player.get_totals(stats_list, game_type=game_type[0], season=season))
-                    elif category == 'averages':
-                        season_data.update(player.get_averages(stats_list, game_type=game_type[0], season=season))
-                    
-                    # Lastly, count how many games the player played
-                    statlines = player.statline_set.filter(game__game_type=game_type[0], game__date__range=(season.start_date, season.end_date))
-                    season_data['gp'] = statlines.count()
-
-                    tables[game_type[1]].append(season_data)
-
-                    if category == 'totals':
-                        for key, value in season_data.items():
-                            if key is not 'title':
-                                if key in overall:
-                                    overall[key] += value
-                                else:
-                                    overall[key] = value
-                    elif category == 'averages':
-                        overall = player.get_averages(stats_list, game_type=game_type[0])
-                        overall['gp'] = ''
-
-            overall_footer[game_type[1]] = overall
-
-        return tables, overall_footer
-
-@register.inclusion_tag('players/stats_tab.html', takes_context=True)
-def player_totals(context, player_id):
-        """Returns a dictionary of totals for a player by Season"""
-
-        totals_tables, overall_footer = calculate_player_overall_dictionaries(context, 'totals', headers.totals_statistics[1:],player_id=player_id)
-        
-        context = {
-            'tables': totals_tables,
-            'overall_footer': overall_footer,
-            'category': 'tot',
-            'active_pill': '5on5',
-            'headers': headers.totals_statistics
-            }
-        
-        return context
-
 
