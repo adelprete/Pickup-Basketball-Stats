77078810ef2004f05600a5695dca644ee631d4eb
Anthony D <adelprete87@gmail.com>
2018-02-06 19:29:52 -0500

    google analytics updated

--- static/build/app.concat.js
+++ static/build/app.concat.js
@@ -119,6 +119,21 @@ angular
     });
 ;'use strict';
 
+angular.module('saturdayBall').run(googleAnalytics);
+
+googleAnalytics.$inject = ['$rootScope', '$location', '$window'];
+
+function googleAnalytics($rootScope, $location, $window) {
+    // initialise google analytics
+    $window.ga('create', 'UA-66942273-1', 'auto');
+
+    // track pageview on state change
+    $rootScope.$on('$routeChangeSuccess', function (event) {
+        $window.ga('send', 'pageview', $location.path());
+    });
+}
+;'use strict';
+
 angular.module('saturdayBall').run(['$rootScope', 'Session', function ($rootScope, Session) {
     $rootScope.session = Session;
 }]);
