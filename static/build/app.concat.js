76eca54d2df68582c0a3363e657bc23867e22d2d
Anthony D <adelprete87@gmail.com>
2018-06-24 11:25:59 -0400

    Forgot to commit bundle

--- static/build/app.concat.js
+++ static/build/app.concat.js
@@ -2035,7 +2035,6 @@ function GroupSettingsController($scope, $routeParams, GroupService, Session,
           $scope.inviteMessage = response.data.message;
         })
       }
-      console.log("ok clicked");
     }
 
     function playerDisplay(id) {
@@ -2046,8 +2045,8 @@ function GroupSettingsController($scope, $routeParams, GroupService, Session,
         return player[0].first_name + ' ' + player[0].last_name
       }
       else {
-        return "None"
-      }
+        return "No Player Selected"
+      } 
     }
 };
 ;'use strict';
